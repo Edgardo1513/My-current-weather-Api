aa3eb3dc5f8bbd6ed7d3be60eb27b3a8defe499b
Edgardo Rafael Oñate Povea <edgardorfael0472@gmail.com>
2023-03-09 15:20:54 -0500

    version1 current weather

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+
+
+
+const NavBar = () => {
+    return(
+        <nav className="navBar bg-dark text-light mb-5">
+            <div className="container-fluid">
+                <h3 className="mx-auto">Current Weather Data</h3>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
